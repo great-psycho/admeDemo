d76c75301fce97c18674f29c7eadbe8857a81492
menuHwang <hmin0206@naver.com>
2022-12-14 12:57:07 +0900

    [Feat] 인증 실패시 로그인 페이지로 리다이렉트

--- src/main/java/com/adme/admedemo/config/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/adme/admedemo/config/security/CustomAuthenticationEntryPoint.java
@@ -1,7 +1,5 @@
 package com.adme.admedemo.config.security;
 
-import com.adme.admedemo.dto.EntryPointErrorResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -17,15 +15,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException ex) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
         log.info("[commence] 인증 실패로 response.sendError 발생");
-
-        EntryPointErrorResponse entryPointErrorResponse = new EntryPointErrorResponse();
-        entryPointErrorResponse.setMsg("인증이 실패하였습니다.");
-
-        response.setStatus(401);
-        response.setContentType("application/json");
-        response.setCharacterEncoding("utf-8");
-        response.getWriter().write(objectMapper.writeValueAsString(entryPointErrorResponse));
+        response.sendRedirect("/user/login");
     }
 }
